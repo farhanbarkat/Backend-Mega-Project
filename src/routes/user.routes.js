d0b7ee6e3643cedcb92e3e6571deda6905ac8df7
Farhan Barkat <farhanbarkat.33@gmail.com>
2026-07-22 00:41:16 +0500

    add jsonwebtoken dependency and fix minor issues in user controller, index, and response utilities

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,7 +7,7 @@ const router = Router()
 router.route("/register").post(
     upload.fields([
         {
-            name : "avator",
+            name : "avatar",
             maxCount: 1
         },
         {
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,9 +1,9 @@
 class ApiError extends Error {
     constructor(
         statusCode,
-        message = " Something went wrong ",
+        message= "Something went wrong",
         errors = [],
-        statck = ""
+        stack = ""
     ){
         super(message)
         this.statusCode = statusCode
@@ -12,13 +12,13 @@ class ApiError extends Error {
         this.success = false;
         this.errors = errors
 
-        if(statck) {
-            this.stack = statck
+        if (stack) {
+            this.stack = stack
         } else{
-            Error.CaptureStackTrace(this, this.constructor)
+            Error.captureStackTrace(this, this.constructor)
         }
-    }
 
+    }
 }
 
-    export {ApiError}
+export {ApiError}
