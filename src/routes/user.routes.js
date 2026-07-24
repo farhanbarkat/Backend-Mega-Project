6d3760d9e4107b6e380874f69b58ae9ec4f0365b
Farhan Barkat <farhanbarkat.33@gmail.com>
2026-07-25 03:50:53 +0500

    add logout functionality and JWT verification middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { registerUser , loginUser } from "../controllers/user.controller.js";
+import { registerUser , loginUser , logoutUser} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 
 const router = Router()
 
@@ -19,5 +21,8 @@ router.route("/register").post(
     registerUser)
 
     router.route("/login").post(loginUser)
+
+    //secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
 
 export default router
